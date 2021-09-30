df9e27e76d25287f9e9b51691e61a0d2208362b7
Sudhanshu Poonia <47912660+sudhnsh@users.noreply.github.com>
2021-09-30 23:47:33 +0530

    Updated the function linear search

--- hacktoberfest2021/linear_search.cpp
+++ hacktoberfest2021/linear_search.cpp
@@ -8,7 +8,14 @@ Task:
 */
 
 int linear_search(vector<int> arr, int k){
-
+    for(int i=0;i<arr.size();i++)
+    {
+        if(arr[i]==k)
+        {
+            return i;
+        }
+    }
+    return -1;
 }
 int main(){
     vector<int> arr;
