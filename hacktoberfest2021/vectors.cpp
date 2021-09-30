3b872f5fecb42418f9d25da76fbde6780f4447ad
Gautam <79162790+mgautam07@users.noreply.github.com>
2021-10-01 00:02:11 +0530

    Updated vectors.cpp

--- hacktoberfest2021/vectors.cpp
+++ hacktoberfest2021/vectors.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 /*
@@ -9,7 +10,11 @@ Task:
 */
 
 int main(){
-
+    vector<int> v = {1, 2, 3, 4, 5};
+    for (auto &it : v)
+    {
+        cout << it << ' ';
+    }
 
     return 0;
 }
