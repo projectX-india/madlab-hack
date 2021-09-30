b0e80ecf3572f03d907cd1c171c422267e977e45
Sudhanshu Poonia <47912660+sudhnsh@users.noreply.github.com>
2021-10-01 00:26:19 +0530

    Update vector.cpp
    updated

--- hacktoberfest2021/vectors.cpp
+++ hacktoberfest2021/vectors.cpp
@@ -9,7 +9,13 @@ Task:
 */
 
 int main(){
-
-
+    vector<int>s;
+    s.push_back(11);
+    s.push_back(22);
+    s.push_back(33);
+    s.push_back(55);
+    for (vector<int>::iterator it = s.begin(); it!=s.end(); it++) {
+    cout << *it << endl;
+}
     return 0;
 }
