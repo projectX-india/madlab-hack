23c996a4f58d31a49d3a1b9ae089f55337ed57a6
Anirudh1331 <62394458+Anirudh1331@users.noreply.github.com>
2021-10-01 00:42:47 +0530

    staircase problem

---
+++ hacktoberfest2021/4. staircase.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+long staircase(int n)
+{
+	//Write your code here
+    long a[n+1];
+    if(n<=1)
+        return 1;
+    else if(n==2)
+        return 2;
+    else if(n==3)
+        return 4;
+    a[0]=1;
+    a[1]=1;
+    a[2]=2;
+    a[3]=4;
+    for(int i=4;i<=n;i++){
+        a[i]=a[i-1]+a[i-2]+a[i-3];
+    }
+    return a[n];
+}
+int main()
+{
+	int n;
+	cin >> n;
+	cout << staircase(n);
+}
