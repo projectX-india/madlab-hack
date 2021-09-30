e9db38d579634c50bdc5d1ab0344fb1aa2120033
varungholap <65026551+varungholap@users.noreply.github.com>
2021-10-01 00:45:04 +0530

    Update classes.cpp

--- hacktoberfest2021/classes.cpp
+++ hacktoberfest2021/classes.cpp
@@ -8,8 +8,35 @@ Task:
 3. it should contain a function to print student details
 */
 
-int main(){
-
-
-    return 0;
+#include<iostream.h>
+#include<fstream.h>
+#include<conio.h>
+class student
+{
+int rno;
+char name[20];
+int age;
+public:
+void get()
+{
+cout<<“\nEnter data: Roll no Name Marks\n”;
+cin>>rno>>name>>age;
+}
+void disp()
+{
+cout<<“\nRoll no\tName\tMarks\n”;
+cout<<“=============================\n”;
+cout<<rno<<“\t”<<name<<“\t”<<age<<endl;
+}
+};
+void main()
+{
+fstream f;
+f.open(“school.dat”,ios::in|ios::out);
+student s;
+s.get();
+f.write((char*)&s,sizeof(s));
+while(f.read((char*)&s,sizeof(s)));
+s.disp();
+getch();
 }
