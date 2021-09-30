50b5e232efbbce505218938a80aaf09c0409d699
Gautam <79162790+mgautam07@users.noreply.github.com>
2021-10-01 00:46:35 +0530

    Updated linked_list.cpp

--- hacktoberfest2021/linked_list.cpp
+++ hacktoberfest2021/linked_list.cpp
@@ -1,14 +1,9 @@
-#include<iostream>
-#include<list>
+#include <iostream>
+#include <list>
 using namespace std;
 
-
-/*
-Task:
-1. implement a linked list and print it
-*/
-
-int main(){
+int main()
+{
     list<int> linkedList;
     for (int i = 0; i < 5; i++)
     {
@@ -22,49 +17,6 @@ int main(){
     {
         cout << it << ' ';
     }
-
-
-
-
-class Node {
-public:
-    int data;
-    Node* next;
-};
- 
-// This function prints contents of linked list
-// starting from the given node
-void printList(Node* n)
-{
-    while (n != NULL) {
-        cout << n->data << " ";
-        n = n->next;
-    }
-}
- 
-// Driver code
-int main()
-{
-    Node* head = NULL;
-    Node* second = NULL;
-    Node* third = NULL;
- 
-    // allocate 3 nodes in the heap
-    head = new Node();
-    second = new Node();
-    third = new Node();
- 
-    head->data = 1; // assign data in first node
-    head->next = second; // Link first node with second
- 
-    second->data = 2; // assign data to second node
-    second->next = third;
- 
-    third->data = 3; // assign data to third node
-    third->next = NULL;
- 
-    printList(head);
- 
 
     return 0;
 }
