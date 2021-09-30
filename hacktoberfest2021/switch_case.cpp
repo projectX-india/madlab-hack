3e66159f7c99000ea63fa0a6c13009693e960bae
Gautam <79162790+mgautam07@users.noreply.github.com>
2021-10-01 01:08:12 +0530

    Update switch_case.cpp

--- hacktoberfest2021/switch_case.cpp
+++ hacktoberfest2021/switch_case.cpp
@@ -1,26 +1,26 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-/*
-Task:
-1. implement switch case in c++
-*/
-
-int main(){
-int test = 3;
-switch (test) {
-  case 1:
-    cout << "Monday";
-    break;
-  case 2:
-    cout << "Tuesday";
-    break;
-  case 3:
-    cout << "Wednesday";
-    break;
-}
-    
-    
+int main()
+{
+    int choice;
+    cout << "Enter your choice : ";
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+        cout << "You entered 1";
+        break;
+    case 2:
+        cout << "You entered 2";
+        break;
+    case 3:
+        cout << "You entered 3";
+        break;
+    default:
+        break;
+    }
 
     return 0;
 }
