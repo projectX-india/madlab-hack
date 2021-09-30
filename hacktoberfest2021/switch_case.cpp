81ecfdaa2fc39b34295e2099cb8d60d16ff63ec6
Prakhar Dhama <pdhama161@gmail.com>
2021-10-01 01:51:36 +0530

    updated switch_case.cpp

--- hacktoberfest2021/switch_case.cpp
+++ hacktoberfest2021/switch_case.cpp
@@ -1,44 +1,47 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-/*
-Task:
-1. implement switch case in c++
-*/
+//switch-case example
 
+void playgame()
+{
+    cout << "Play game called";
+}
+void loadgame()
+{
+    cout << "Load game called";
+}
+void playmultiplayer()
+{
+    cout << "Play multiplayer game called";
+}
+     
 int main()
 {
-int n;
-  cout<<"Enter any number between(1-7):";
-  cin>>n;
-  switch(n)
-  {
-    case 1:
-      cout<<"Monday"<<endl;
-      break;
-    case 2:
-      cout<<"Tuesday"<<endl;
-      break;
-    case 3:
-      cout<<"Wednesday"<<endl;
-      break;
-    case 4:
-      cout<<"Thursday"<<endl;
-      break;
-    case 5:
-      cout<<"Friday"<<endl;
-      break;
-    case 6:
-      cout<<"Saturday"<<endl;
-      break;
-    case 7:
-      cout<<"Sunday"<<endl;
-      break;
-    default:
-      cout<<"Invalid input"<<endl;
-      break;
+  int input;
+   
+  cout<<"1. Play game\n";
+  cout<<"2. Load game\n";
+  cout<<"3. Play multiplayer\n";
+  cout<<"4. Exit\n";
+  cout<<"Selection: ";
+  cin>> input;
+  switch ( input ) {
+  case 1:            
+    playgame();
+    break;
+  case 2:            
+    loadgame();
+    break;
+  case 3:            
+    playmultiplayer();
+    break;
+  case 4:            
+    cout<<"Thank you for playing!\n";
+    break;
+  default:            
+    cout<<"Error, bad input, quitting\n";
+    break;
   }
-  
-
-    return 0;
+  cin.get();
 }
