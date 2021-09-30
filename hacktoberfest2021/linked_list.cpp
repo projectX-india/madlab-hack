f13aa3557e0a8c477ef4e0e3ef4977197da1ef83
Gautam <79162790+mgautam07@users.noreply.github.com>
2021-10-01 00:32:05 +0530

    Updated linked_list.cpp

--- hacktoberfest2021/linked_list.cpp
+++ hacktoberfest2021/linked_list.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<list>
 using namespace std;
 
 /*
@@ -7,6 +8,20 @@ Task:
 */
 
 int main(){
+    list<int> linkedList;
+    for (int i = 0; i < 5; i++)
+    {
+        int element;
+        cout << "Enter an element to add to the Linked List : ";
+        cin >> element;
+        linkedList.push_back(element);
+    }
+    cout << "The linked list is :- ";
+    for (auto &it : linkedList)
+    {
+        cout << it << ' ';
+    }
+
 
 
     return 0;
