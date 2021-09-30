30bad6563da125f61e876d3e331de9bc84c8504c
Gautam <79162790+mgautam07@users.noreply.github.com>
2021-10-01 00:21:39 +0530

    Updated switch-case.cpp

--- hacktoberfest2021/switch_case.cpp
+++ hacktoberfest2021/switch_case.cpp
@@ -7,7 +7,23 @@ Task:
 */
 
 int main(){
-
+    int choice;
+    cout << "Enter your choice : ";
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+        cout << "You entered 1";
+        break;
+    case 2:
+        cout << "You entered 2";
+        break;
+    case 3:
+        cout << "You entered 3";
+        break;
+    default:
+        break;
+    }
 
     return 0;
 }
