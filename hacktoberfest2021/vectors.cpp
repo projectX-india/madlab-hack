fbe217652d1538a2eb61f5aeccac812f095fb8be
aditya ranaut <ranout.aditya@gmail.com>
2021-09-30 23:51:58 +0530

    Update vectors.cpp

--- hacktoberfest2021/vectors.cpp
+++ hacktoberfest2021/vectors.cpp
@@ -6,10 +6,10 @@ Task:
 1. implement a vector in c++
 2. initialize the vector with values
 3. iterate over the vector using the auto keyword
+4. finally create a pull request for hacktoberfest
 */
 
 int main(){
-
 
     return 0;
 }
