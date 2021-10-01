ba09ee8d2945142d9e66ab6d0b925ab135c02e68
Rachana <71558587+rachana1009@users.noreply.github.com>
2021-10-01 19:12:01 +0530

    Updated linear search

--- hacktoberfest2021/linear_search.cpp
+++ hacktoberfest2021/linear_search.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 using namespace std;
  
-int linearSearch(int arr[], int n, int x) {
-    for (int i = 0; i < n; i++){
-        if (arr[i] == x){
-            return i;
-        }
-    }
-    return -1;
+int linearSearch(int arr[],int num,int ele)
+{
+ for(int i=0;i<num;i++)
+ {
+  if(arr[i]==ele)
+   return i;
+ }
+ return -1;
 }
 
-int main() {
-    int n,x;
-    cin >> n >> x;
-
-    int arr[n];
-
-    for(int i=0;i<n;i++){
-        cin >> arr[i];
-    }
-   
-    int result = linearSearch(arr, n, x);
-    if(result == -1){
-        cout << "Element is not present in array" << endl;
-    }
-    else{
-        cout << "Element is present at index " << result << endl;
-    }
-    return 0;
+int main()
+{
+ int n,ele;
+ cout<<"Number of elements you want to enter in an array:";
+ cin>>n;
+ int arr[n];
+ cout<<"Enter the elements:";
+ for(int j=0;j<n;j++)
+  cin>>arr[j];
+ cout<<"Enter the element you want to search:";
+ cin>>ele;
+ int res=linearSearch(arr,n,ele);
+ if(res==-1)
+  cout<<"Element not found!"<<endl;
+ else
+  cout<<"Element found at index:"<<res<<endl;
+ return 0;
 }
