a7e0a2acbca7614630400e8b4827f42bbcd7bffc
Aviral1-jain <74827110+Aviral1-jain@users.noreply.github.com>
2021-10-02 00:02:01 +0530

    Update Binary_search.cpp

--- hacktoberfest2021/binary_search.cpp
+++ hacktoberfest2021/binary_search.cpp
@@ -1,99 +1,35 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
-//declaring node class
-class node{
-public:
-	int data;
-	node* left;
-	node* right;
 
-	node(int d){
-		data = d;
-		left = NULL;
-		right = NULL;
+int binarysearch(int arr[],int n,int key){
+	int s=0;
+	int e=n;
+	while(s<=e){
+		int mid=(s+e)/2;
+		if(arr[mid]==key){
+			return mid;
+		}
+		else if(arr[mid]>key){
+			e=mid-1;
+		}
+		else{
+			s=mid+1;
+		}
 	}
-};
-
-//function to insert in BST
-node* insertInBST(node* root, int d)
-{
-	if(root == NULL)
-		return new node(d);
-
-	if(d > root -> data)
-		root -> right = insertInBST(root -> right, d);
-	else
-		root -> left = insertInBST(root -> left, d);
-
-	return root;
+	return -1;
 }
-//buyilding BST
-node* buildBST()
-{
-	int d;
-	cin >> d;
+int main(){
 	
-	node* root = NULL;
-
-	while(d != -1)
-	{
-		root = insertInBST(root, d);
-		cin>>d;
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
 	}
-	return root;
-}
-
-void bfsNewline(node* root)
-{
-	queue<node*> q;
-	q.push(root);
-	q.push(NULL);
-	while(!q.empty())
-	{
-		node* f = q.front();
-		if(f == NULL)
-		{
-			cout<<"\n";
-			q.pop();
-			if(!q.empty())
-				q.push(NULL);
-		}
-		else
-		{
-			cout<<f -> data<<", ";
-			q.pop();
-
-			if(f -> left)
-				q.push(f -> left);
-			if(f -> right)
-				q.push(f -> right);
-		}
-	}
-	return;
-}
-
-void inorderPrint(node* root)
-{
-	if(root == NULL)
-		return;
-	inorderPrint(root -> left);
-	cout<<root->data<<", ";
-	inorderPrint(root -> right);
-}
-
-istream& operator>>(istream &is, node*&root)
-{
-	root = buildBST();
-	return is;
-}
-
-int main()
-{
-	// node* ROOT = buildBST();
-	node* ROOT = NULL;
-	cin>>ROOT;
-	inorderPrint(ROOT);
-	cout<<"\n\n";
-	bfsNewline(ROOT);
+	int key;
+	cin>>key;
+	cout<<binarysearch(arr,n,key)<<endl;
+	
 	return 0;
+	
 }
