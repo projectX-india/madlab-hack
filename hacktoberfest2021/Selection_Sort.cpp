db7d670ce11fe2a06463ac68685bcb5c0b6ee6e9
Mirang1111 <83184604+Mirang1111@users.noreply.github.com>
2021-10-03 01:03:12 +0530

    Update Selection_Sort.cpp

--- hacktoberfest2021/Selection_Sort.cpp
+++ hacktoberfest2021/Selection_Sort.cpp
@@ -4,50 +4,33 @@
     Selection Sort
     in C++
 */
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
- 
-void swap(int &a, int &b)
-{
-    int temp = a;
-    a = b;
-    b = temp;
-}
-
-//Selection Sort function
-//finding the minimum element
-//and swapping with the first element
-void Selection_Sort(int arr[], int n)
-{
-    int min_ind;
-
-    for (int i = 0; i < n-1; i++)
-    {
-        min_ind = i;
-        for (int j = i+1; j < n; j++)
-        {
-            if (arr[j] < arr[min_ind])
-                min_ind = j;
-        }
-        swap(arr[min_ind], arr[i]);
-    }
-}
-
-void print(int arr[], int len)
-{
-    for (int i=0; i < len; i++)
-        cout<< arr[i] << " ";
-    cout<< endl;
-}
-
-
 int main()
 {
-    int arr[] = {23, 78, 11, 90, 1};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    Selection_Sort(arr, n);
-    cout << "Sorted array: \n";
-    print(arr, n);
-    return 0;
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0 ;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    
+    for(int i = 0 ; i<n-1;i++)
+    {
+        for(int j = i+1;j<n;j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    cout<<"Sorted array is "<<endl;
+    for(int i = 0 ; i<n;i++)
+    {
+        cout<<arr[i];
+    }
 }
