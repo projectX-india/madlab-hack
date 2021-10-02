9ae9da85cc1d0a9aaffe3c1747e4a10e71d1414f
Mirang1111 <83184604+Mirang1111@users.noreply.github.com>
2021-10-03 02:37:05 +0530

    Update heapSort.c

--- hacktoberfest2021/heapSort.c
+++ hacktoberfest2021/heapSort.c
@@ -7,18 +7,6 @@ void swap(int* a, int* b)
     *b = temp;
 }
 
-void heap_sort(int arr[], int n)
-{
-    int i;
-    for (i = (n / 2) - 1; i >= 0; i--)
-        heapify(arr, n, i);
-
-    for (i = n - 1; i >= 0; i--) {
-        swap(&arr[0], &arr[i]); 
-        heapify(arr, i, 0); 
-    }
-}
-
 void heapify(int arr[], int n, int i)
 {
     int left, right, largest;
@@ -26,34 +14,45 @@ void heapify(int arr[], int n, int i)
     left = 2 * i + 1;
     right = 2 * i + 2;
 
+    // Check if left child exists and is larger than its parent
     if (left < n && arr[left] > arr[largest])
         largest = left;
-
+    // Check if right child exists and larger than its parent
     if (right < n && arr[right] > arr[largest])
         largest = right;
 
+    // if root is not the largest
     if (largest != i) {
-        swap(&arr[i], &arr[largest]);
-        heapify(arr, n, largest); 
+        swap(&arr[i], &arr[largest]); 
+        heapify(arr, n, largest); // Apply heapify to the largest node
     }
 }
 
+void heap_sort(int arr[], int n)
+{
+    int i;
+    for (i = (n / 2) - 1; i >= 0; i--)
+        heapify(arr, n, i);
 
+    for (i = n - 1; i >= 0; i--) {
+        swap(&arr[0], &arr[i]); //Move the largest element at root to the end
+        heapify(arr, i, 0); //Apply heapify to reduced heap
+    }
+}
 
 int main()
 {
-    int N;
-	printf("\nEnter number of elements:");
-	scanf("%d",&N);
-	int arr[N];
-	printf("\nEnter elements:");
-    for(int i=0;i<N;i++)
-		scanf("%d",&arr[i]);
-		
-    heap_sort(arr,N);
+    int arr[] = { 20, 13, 34, 56, 12, 10 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Array:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+        
+    heap_sort(arr, n);
 
     printf("\nAfter performing Heap Sort:\n");
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
 
     return 0;
