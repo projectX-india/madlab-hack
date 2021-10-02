f5a72b385ef54102b047c221f2738fbc667a437b
Mirang1111 <83184604+Mirang1111@users.noreply.github.com>
2021-10-03 03:35:44 +0530

    Update 6. countSort.cpp
    Code sort code in c++ language

--- hacktoberfest2021/6. countSort.cpp
+++ hacktoberfest2021/6. countSort.cpp
@@ -1,26 +1,38 @@
-#include <iostream> 
-#include <string.h> 
-using namespace std; 
-#define RANGE 255  
-void countSort(char arr[]) 
-{  
-    char output[strlen(arr)]; 
-    int count[RANGE + 1], i; 
-    memset(count, 0, sizeof(count)); 
-    for (i = 0; arr[i]; i++) 
-        count[arr[i]]++; 
-    for (i = 1; i <= RANGE; i++) 
-        count[i] += count[i - 1]; 
-  	for (i = 0; arr[i]; ++i) { 
-        output[count[arr[i]] - 1] = arr[i]; 
-        count[arr[i]]--; 
-    }  
-    for (i = 0; arr[i]; i++) 
-        arr[i] = output[i]; 
+#include<bits/stdc++.h>
+using namespace std;
+void countSort(vector<int>& arr)
+{
+    int max = *max_element(arr.begin(), arr.end());
+    int min = *min_element(arr.begin(), arr.end());
+    int range = max - min + 1;
+ 
+    vector<int> count(range), output(arr.size());
+    for (int i = 0; i < arr.size(); i++)
+        count[arr[i] - min]++;
+ 
+    for (int i = 1; i < count.size(); i++)
+        count[i] += count[i - 1];
+ 
+    for (int i = arr.size() - 1; i >= 0; i--) {
+        output[count[arr[i] - min] - 1] = arr[i];
+        count[arr[i] - min]--;
+    }
+ 
+    for (int i = 0; i < arr.size(); i++)
+        arr[i] = output[i];
 }
-int main() { 
-    char arr[] = "datastructuresandalgorithms"; 
-    countSort(arr); 
-    cout << "Sorted array is \n" << arr; 
-    return 0; 
+ 
+void printArray(vector<int>& arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+ 
+int main()
+{
+    vector<int> arr = { -5, -10, 0, -3, 8, 5, -1, 10 };
+    countSort(arr);
+    printArray(arr);
+    return 0;
 }
