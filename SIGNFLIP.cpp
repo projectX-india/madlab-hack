f0758cd468969146d99ab71ecc4f36868f4e1b10
Mirang1111 <83184604+Mirang1111@users.noreply.github.com>
2021-10-03 02:43:27 +0530

    Update SIGNFLIP.cpp

--- SIGNFLIP.cpp
+++ SIGNFLIP.cpp
@@ -1,43 +1,38 @@
 #include <bits/stdc++.h>
-#define ll long long
+
 using namespace std;
 
+#define endl '\n'
+#define ll long long
+
+void solve() {
+    ll n, k, total = 0;
+    cin >> n >> k;
+    vector<ll> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+        if (a[i] > 0) {
+            total += a[i];
+        }
+    }
+    sort(a.begin(), a.end());
+    for (int i = 0; i < n; ++i) {
+        if (a[i] >= 0 || k <= 0) {
+            break;
+        } else {
+            total -= a[i];
+            k--;
+        }
+    }
+    cout << total << endl;
+}
+
 int main() {
-	// write your code goes here
-	int t;
-	cin>>t;
-	
-	while(t--){
-	    int n,k;
-	    cin>>n>>k;
-	    
-	    int f=0;
-	    int arr[n];
-	    for(int i=0;i<n;i++){
-	        cin>>arr[i];
-	    }
-	    
-	    sort(arr,arr+n);
-	    
-	   int i=0;
-	    while(i<k){
-	        if(arr[i]<0)
-	        arr[i]=arr[i]*-1;
-	        i++;
-	    }
-	   
-	    
-	    
-	    int sum=0;
-	    for(int i=0;i<n;i++){
-	        if(arr[i]>0){
-	            sum+=arr[i];
-	        }
-	        
-	       
-	    }
-	    
-	    cout<<sum<<endl;
-	}
-	return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll tt;
+    cin >> tt;
+    for (int i = 0; i < tt; i++) {
+        solve();
+    }
 }
