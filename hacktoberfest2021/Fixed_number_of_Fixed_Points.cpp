2f6d55b6c394a7dbf9e3102d1b4a6cad803d91b9
Sunny <72219493+SINISTER-code@users.noreply.github.com>
2021-10-05 13:35:50 +0530

    Update Fixed_number_of_Fixed_Points.cpp

--- hacktoberfest2021/Fixed_number_of_Fixed_Points.cpp
+++ hacktoberfest2021/Fixed_number_of_Fixed_Points.cpp
@@ -1,36 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
 using namespace std;
 
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n, k;
-        cin>>n>>k;
-
-        if(n==k){
-            for(int i = 1; i <= n; i++){
-                cout<<i<<" "<<endl;
-            }
-        }
-
-        else if(k==n-1){
-            cout<<"-1"<<endl;
-        }
-
-        else{
-            for(int i = 1; i <= k; i++){
-                cout<<i<<" ";
-            }
-
-            for(int i = k+2; i<=n; i++){
-                cout<<i<<" ";
-            }
-
-            cout<<k+1<<endl;
-        }
-
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    if (k == n - 1) {
+        cout << -1 << '\n';
+        return;
     }
+    for (int i = 1; i <= k; i++) 
+    {
+        cout << i << ' ';
+    }
+    for (int j = k + 2; j <= n; j++) 
+    {
+        cout << j << ' ';
+    }
+    if (k < n) cout << k + 1 << '\n';
+    
+    else cout << '\n';
+}
 
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--) 
+    solve();
     return 0;
 }
