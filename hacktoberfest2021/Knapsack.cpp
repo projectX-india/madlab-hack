874fdac3c1f01c4258383dd514c914b869a092fb
7SmD <80188078+7SmD@users.noreply.github.com>
2021-10-08 11:57:09 +0530

    Update Knapsack.cpp

--- hacktoberfest2021/Knapsack.cpp
+++ hacktoberfest2021/Knapsack.cpp
@@ -1,26 +1,40 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
-int knapsack(int value[],int wt[],int n,int W)
+int knapsack(int *weights, int *values, int n, int maxWeight)
 {
-    if(n==0 || W==0)
-    {
+    if (maxWeight == 0 || n == 0)
         return 0;
+    if (weights[0] <= maxWeight)
+    {
+        int a = values[0] + knapsack(weights + 1, values + 1, n - 1, maxWeight - weights[0]);
+        int b = knapsack(weights + 1, values + 1, n - 1, maxWeight);
+        return max(a, b);
     }
-    if(wt[n-1]>W){
-        return knapsack(value,wt,n-1,W);
+    else
+    {
+        return knapsack(weights + 1, values + 1, n - 1, maxWeight);
     }
-    return max(knapsack(value,wt,n-1,W-wt[n-1])+value[n-1],knapsack(value,wt,n-1,W));
 }
 
 int main()
 {
-    int wt[]={10,20,30};
-    int value[]={100,50,150};
-    int W=50;
+    int n;
+    cin >> n;
+    int *weights = new int[n];
+    int *values = new int[n];
 
-    cout<<knapsack(value,wt,3,W);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> weights[i];
+    }
 
-    return 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> values[i];
+    }
 
+    int maxWeight;
+    cin >> maxWeight;
+
+    cout << knapsack(weights, values, n, maxWeight) << endl;
 }
