5d836c2baed499bd8bbf2909f1e3aab2b8d3fbdb
tanya1191 <76492020+tanya1191@users.noreply.github.com>
2021-10-12 00:09:57 +0530

    Update switch_case.cpp

--- hacktoberfest2021/switch_case.cpp
+++ hacktoberfest2021/switch_case.cpp
@@ -5,69 +5,32 @@ using namespace std;
 
 //switch-case example
 
-void IAB()
-{
-    cout<<"Insertion At the Beginning"<<endl;
-}
+int main() {
+    char oper;
+    float num1, num2;
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> oper;
+    cout << "Enter two numbers: " << endl;
+    cin >> num1 >> num2;
 
-void IAE()
-{
-    cout<<"Insertion At the End"<<endl;
-}
-
-void IAI()
-{
-    cout<<"Insertion At the Index"<<endl;
-}
-void DAB()
-{
-    cout<<"Deletion At the Beginning"<<endl;
-}
-void DAE()
-{
-    cout<<"Deletion At the End"<<endl;
-}
-void DAI()
-{
-    cout<<"Deletion At the Index"<<endl;
-}
-
-int main()
-{
-    int ch;
-    string c = "y";
-    while (c=="y")
-    {
-       cout<<"1. IAB\n2. IAE\n3. IAI\n4.DEB\n5.DAE\n6.DAI\n";
-       cout<<"Enter your choice"<<endl;
-       cin>>ch;
-       switch (ch)
-       {
-       case 1:
-           IAB();
-           break;
-       case 2:
-           IAE();
-           break;
-       case 3:
-           IAI();
-           break;
-       case 4:
-           DAB();
-           break;
-       case 5:
-           DAE();
-           break;
-       case 6:
-           DAI();
-           break;
-       default:
-           cout<<"Wrong choice"<<endl;
-           break;
-       }
-       cout<<"Do you want to continue(y/n)"<<endl;
-       cin>>c;
+    switch (oper) {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+        case '/':
+            cout << num1 << " / " << num2 << " = " << num1 / num2;
+            break;
+        default:
+            // operator is doesn't match any case constant (+, -, *, /)
+            cout << "Error! The operator is not correct";
+            break;
     }
-    
+
     return 0;
 }
