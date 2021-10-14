9b4201d3ed1053addd53d9b579968b9102299743
phoenix-aditya <ranout.aditya@gmail.com>
2021-10-15 01:29:31 +0530

    hacktoberfest2021_goodbye

--- hacktoberfest2021/lcs.cpp
+++ hacktoberfest2021/lcs.cpp
@@ -1,48 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include <bits/stdc++.h>
-using namespace std;
-int LCS(string str1, string str2)
+ 
+int max(int a, int b);
+ 
+/* Returns length of LCS for X[0..m-1], Y[0..n-1] */
+int lcs( char *X, char *Y, int m, int n )
 {
-  int length1 = str1.size();
-  int lenght2 = str2.size();
-  int dp[length1 + 1][lenght2 + 1];	// Declared the DP 2-D matrix ;
-  for (int i = 0; i <= length1; i++)
-    {
-    for (int j = 0; j <= lenght2; j++)
-	{
-	    if (i == 0 || j == 0)
-	         {
-	              dp[i][j] = 0;	//If one of the string length becomes 0 LCS will be 0 ;
-		 }
-	     else if (str1[length1 - i] == str2[lenght2 - j])
-	           {
-	                dp[i][j] = 1 + dp[i - 1][j - 1];	// when the first Character Matches 
-	               }
-	      else
-		 {
-	      int a = dp[i - 1][j];	// when first Character of first string is considered in the LCS solution .
-               int b = dp[i][j - 1];	// when first Character of second string is considered in the LCS solution .
-               int c = dp[i - 1][j - 1];	// when none of the first character is considered in the solution .
-              dp[i][j] = max(a, max(b, c));
-			}
-		}
-	}
-   return dp[length1][lenght2];
+    if (m == 0 || n == 0)
+        return 0;
+    if (X[m-1] == Y[n-1])
+        return 1 + lcs(X, Y, m-1, n-1);
+    else
+        return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n));
 }
-int main()    
+ 
+/* Utility function to get max of 2 integers */
+int max(int a, int b)
 {
-	string str1, str2;
-	cin >> str1;
-	cin >> str2;
-	cout << "LCS of " << str1 << " and " << str2 << " is " << LCS(str1, str2) << endl;
-	return 0;
+    return (a > b)? a : b;
 }
-int main()    
+ 
+/* Driver code */
+int main()
 {
-	string str1, str2;
-	cin >> str1;
-	cin >> str2;
-	cout << "LCS of " << str1 << " and " << str2 << " is " << LCS(str1, str2) << endl;
-	return 0;
+    char X[] = "AGGTAB";
+    char Y[] = "GXTXAYB";
+     
+    int m = strlen(X);
+    int n = strlen(Y);
+     
+    cout<<"Length of LCS is "<< lcs( X, Y, m, n ) ;
+     
+    return 0;
 }
