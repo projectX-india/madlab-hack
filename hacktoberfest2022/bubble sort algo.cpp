c5303db9976b07ad092590ffda86a363e8c1ef8f
Aditya Ranaut <ranout.aditya@gmail.com>
2022-10-01 00:41:11 +0530

    Added dsa codes

---
+++ hacktoberfest2022/bubble sort algo.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define pi 3.14159265359
+#define ll long long
+#define wh(t) int t;cin>>t; while(t--)
+#define speed  ios::sync_with_stdio(0); cin.tie(0);
+#define endl "\n"
+#define f(i,a,b) for(int i=a;i<b;i++)
+using namespace std;
+#include<string.h>
+
+void bubblesort(int arr[], int n)
+{
+    for(int i=0;i<n;i++)
+//        first i elements already in place
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+                swap(arr[j], arr[j+1]);
+        }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    
+    bubblesort(arr, n);
+    
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<" ";
+    cout<<endl;
+    return 0;
+}
