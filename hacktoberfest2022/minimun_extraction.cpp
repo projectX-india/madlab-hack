86f3f51c7b95a7210530abcc80fdf594d05e2f9f
Tilak Sovan Khatua <62074842+tilak-codes@users.noreply.github.com>
2022-10-01 11:40:26 +0530

    Create minimun_extraction.cpp

---
+++ hacktoberfest2022/minimun_extraction.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 10000
+#define ll long long 
+int main() {
+    int tt;
+    cin>>tt;
+    while(tt--) {
+        ll n;
+        cin>>n;
+        ll arr[n];
+        for(int i=0;i<n;i++) {
+            cin>>arr[i];
+        }
+        sort(arr,arr+n);
+        ll curr = 0;
+        ll ans = arr[0];
+        for(int i=0;i<n-1;i++) {
+            if(arr[i]>=0)
+                curr+=arr[i];
+            else
+                curr += arr[i];
+            ans = max(ans , arr[i+1] - curr);
+            arr[i+1] -= curr;
+        }
+        cout<<ans<<endl;
+        
+    }
+}
